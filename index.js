c920d76af748e3288e258a45b3a93b17ec8c6fd6
CaseyRayson1995 <caseyrayson1995@gmail.com>
2024-09-17 05:48:21 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+// index.js companyName  is set as Scuber ✓
+//index.js companyName is defined as a const ✓ (as shown in code directly below)
+const companyName = "Scuber";
+
+
+//mostProfitableNeighborhood is declared as equal to Chelsea ✓ is defined using let ✓
+//(the line of code you had originally it did not pass both test because you forgot tot put chelsea in "" and you forgot the ;)
+//let mostProfitableNeighborhood = Chelsea 
+let mostProfitableNeighborhood = "Chelsea";
+
+
+//companyCeo is declared as equal to Susan Smith ✓ is defined using let ✓(as shown below)
+let companyCeo = "Susan Smith";
+
+
